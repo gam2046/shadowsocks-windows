6e831e785b825f80ea89a07d32c2957eee4533d7
forDream <gam2046@gmail.com>
2016-10-29 17:51:15 +0800

    覆盖ToString方法，便于调试信息输出

---
+++ shadowsocks-csharp/Model/ServerEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shadowsocks.Model
+{
+    class ServerEx :Server
+    {
+        public override string ToString()
+        {
+            return new StringBuilder().Append("Server:\t").Append(this.server).Append("\t")
+                 .Append("Port:\t").Append(this.server_port).Append("\t")
+                 .Append("Pwd:\t").Append(this.password).Append("\t")
+                 .Append("Method:\t").Append(this.method)
+                 .ToString();
+        }
+    }
+}
